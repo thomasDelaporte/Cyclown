96c18e3f437bd03852b3152e45e5a6bedcffa535
Thomas Delaporte <thomas.delaporte30@gmail.com>
2021-01-16 18:36:31 +0100

    Storybook test
    + Github actions test
    + Default storybook init

---
+++ src/stories/Scrollbar.stories.js
@@ -0,0 +1,13 @@
+// Scrollbar.stories.js
+
+import React from 'react';
+
+export const Ah = () => <h1>oui</h1>
+
+export default {
+  title: 'Example/Scrollbar'
+}
+
+export const functiontest = () => {
+  return <h1>ah</h1>
+}
