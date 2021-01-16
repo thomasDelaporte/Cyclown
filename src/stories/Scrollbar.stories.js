4e26245877dc99fde2bf359556786cdad5b3d9e2
Thomas Delaporte <thomas.delaporte30@gmail.com>
2021-01-16 18:43:00 +0100

    Update Scrollbar.stories.js

---
+++ src/stories/Scrollbar.stories.js
@@ -0,0 +1,17 @@
+// Scrollbar.stories.js
+
+import React from 'react';
+
+export const Ah = () => <h1>oui</h1>
+
+export default {
+  title: 'Example/Scrollbar'
+}
+
+export const functiontest = () => {
+  return <h1>ah</h1>
+}
+
+export const again = () => {
+  return <h2>again</h2>
+}
